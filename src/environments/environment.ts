17f603239b946dff6da687b4347f6456cd155a72
navidmirzaie <navid.ncreative@gmail.com>
2023-11-01 12:14:27 +0100

    moved endpoints to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,15 +7,12 @@ export const environment = {
   posterUrl: "https://image.tmdb.org/t/p/original",
   apiUrl: "https://api.themoviedb.org/3",
   apiKey: "1563b464da8d37cf88cd5eaec8aa5419",
+  popular: `https://api.themoviedb.org/3/movie/popular`,
+  topRated: `https://api.themoviedb.org/3/movie/top_rated`,
+  upcoming: `https://api.themoviedb.org/3/movie/upcoming`,
+  nowPlaying: `https://api.themoviedb.org/3/movie/now_playing`,
+  genres: `https://api.themoviedb.org/3/genre/movie/list`
 };
-
-export const endpoints = {
-  popular: `${environment.apiUrl}/movie/popular`,
-  topRated: `${environment.apiUrl}/movie/top_rated`,
-  upcoming: `${environment.apiUrl}/movie/upcoming`,
-  nowPlaying: `${environment.apiUrl}/movie/now_playing`,
-  genres: `${environment.apiUrl}/genre/movie/list`
-}
 
 /*
  * For easier debugging in development mode, you can import the following file
